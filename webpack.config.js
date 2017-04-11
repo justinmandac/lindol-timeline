3a16be2e3ce309e9e671fc2928e55bbf9f6c93ce
Bien Mandac <justinmandac@gmail.com>
2017-04-11 15:25:16 +0800

    Completed basic webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,27 @@ module.exports = {
                 exclude: [
                     path.resolve(__dirname, 'node_modules')
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader!sass-loader'
+                })
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/i,
+                loader: 'file-loader?name=/img/[name].[ext]'
             }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ],
+    resolve: {
+        modules: [
+            path.resolve('./src'),
+            path.resolve('./node_modules')
         ]
     }
 
