657c2ab04836f316d08b65b01e13d2a6a67cf1b3
Bien Justin Mandac <justinmandaca@gmail.com>
2017-04-11 20:49:41 +0800

    Load data from USGS

--- src/app/api.js
+++ src/app/api.js
@@ -1,3 +1,33 @@
-const BASE_URL = 'https://earthquake.usgs.gov/fdsnws/event/1/query'
-const FORMAT = 'format=geojson'
+const URL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?' +
+            'format=geojson&' +
+            'orderby=time&' +
+            'eventtype=earthquake&' +
+            'latitude=14.5995&' +
+            'longitude=120.9842&' +
+            'maxradiuskm=1000';
+
+export default function loadData() {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', URL);
+
+    xhr.onload = ({ target }) => {
+      const { status, response } = target;
+
+      if (status >= 200 && status < 400) {
+        const responseParsed = JSON.parse(response);
+        resolve(responseParsed);
+      } else {
+        reject(target);
+      }
+    };
+
+    xhr.onerror = ({ target }) => {
+      reject(target);
+    };
+
+    xhr.send();
+  });
+}
 
