ab22f749eec4797c2c32a3396af0510d4cc28114
Bien Mandac <justinmandac@gmail.com>
2017-04-11 15:21:36 +0800

    Initial webpack setup for JS files

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,23 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 module.exports = {
+    entry : './src/app/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
+                ]
+            }
+        ]
+    }
 
 }
