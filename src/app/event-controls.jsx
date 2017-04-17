f951b4bb2f4689269eb12fd0ac39856dea594876
Bien Mandac <justinmandac@gmail.com>
2017-04-17 14:43:43 +0800

    Added slider control

--- src/app/event-controls.jsx
+++ src/app/event-controls.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 const EventControls = (props) => <div className="event-details__controls" onChange={props.onChange}>
     <input type="radio" id="timeSelector-7d" name="timeSelection" value="7" /> 7 Days Ago
     <input type="radio" id="timeSelector-30d" name="timeSelection" value="30" /> 30 Days Ago
+
+    <input type="range" min="0" max="100" step="1" />
 </div>
 
 export default EventControls;
