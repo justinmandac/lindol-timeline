36f88918319e8044ba0612c333fd8eec7fb8b593
justinmandac <justinmandaca@gmail.com>
2017-04-18 21:44:37 +0800

    Added injectTapEventPlugin

--- src/main.js
+++ src/main.js
@@ -6,13 +6,19 @@ import ReactDOM from 'react-dom';
 
 import App from 'app/app.jsx';
 import API from 'app/api';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // TODO: Move API call to app and enable acquisition of events 100 days prior to current date
 const lat = 12.8797; // PH Latitude
 const lng = 121.7740; // PH Longitude
 const radius = 942; // Approx radius of PH around coords. Used the sqrt of PH area as radius
 // plus a ~200km fudge factor
 let ref;
-const AppRender = <App lat={lat} lng={lng} ref={ (app) => { ref = app; } }/>;
+const AppRender = <MuiThemeProvider>
+    <App lat={lat} lng={lng} ref={ (app) => { ref = app; } }/>
+</MuiThemeProvider>;
+
+injectTapEventPlugin();
 
 API(lat, lng, radius).then((data) => {
     ref.setData(data);
