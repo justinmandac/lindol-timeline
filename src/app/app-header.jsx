38f1561978e97adef9e643b08a4000fc842b2b79
Bien Mandac <justinmandac@gmail.com>
2017-04-21 10:45:05 +0800

    Added menu icon

--- src/app/app-header.jsx
+++ src/app/app-header.jsx
@@ -4,10 +4,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Paper from 'material-ui/Paper';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui/svg-icons/navigation/menu';
 
 import getDaysAgo from './utils/date-formatter';
 
 const EarthquakeAppHeader = props => <header className="header">
+  <IconButton className="menu-icon">
+    <MenuIcon />
+  </IconButton>
   <Paper className="card header__date-card">
     {getDaysAgo(props.value)}
   </Paper>
