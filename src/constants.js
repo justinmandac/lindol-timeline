d5ae245f98db006acceb361dc9ff186818c4a28b
Bien Mandac <justinmandac@gmail.com>
2017-08-18 11:38:05 +0800

    Created a constants file

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+/**
+ * @fileoverview Constants and default values.
+ */
+
+export const PH_CENTER_LONG = 121.7740;
+
+export const PH_CENTER_LAT = 12.8797;
+
+// Approx radius of PH around coords. Used the sqrt of PH area as radius
+// plus a ~200km fudge factor
+export const DEFAULT_RADIUS = 942;
+
+export const MAPBOX_TOKEN =
+  'pk.eyJ1IjoianVzdGlubWFuZGFjIiwiYSI6ImNqNmczc3FyajA1NHgycHBrNzlneW9oMG0ifQ.eF_q_8CkR4-vlGhBAMqhtw';
+
+export const MAP_BOX_STYLE = 'mapbox://styles/mapbox/dark-v9';
