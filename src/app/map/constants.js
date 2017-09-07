37cf4aed62f275a23e895d39573f9b0b517bf27c
Bien Justin Mandac <justinmandac@gmail.com>
2017-09-07 17:49:46 +0800

    Moved mapbox-specific from main constants file to map/constants

---
+++ src/app/map/constants.js
@@ -0,0 +1,7 @@
+/**
+ * @fileoverview Place map specific constants here. 
+ */
+export const MAPBOX_TOKEN =
+'pk.eyJ1IjoianVzdGlubWFuZGFjIiwiYSI6ImNqNmczc3FyajA1NHgycHBrNzlneW9oMG0ifQ.eF_q_8CkR4-vlGhBAMqhtw';
+
+export const MAP_BOX_STYLE = 'mapbox://styles/mapbox/dark-v9';
