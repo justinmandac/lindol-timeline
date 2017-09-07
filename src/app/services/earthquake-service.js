b89caf0eda3817d0ea9f7428fcc5b259ee166968
Bien Justin Mandac <justinmandac@gmail.com>
2017-09-07 17:04:08 +0800

    Removed api.js and created a dedicated services/ dir; created earthquake-service in its place

---
+++ src/app/services/earthquake-service.js
@@ -0,0 +1,51 @@
+const URL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?' +
+'format=geojson&' +
+'orderby=time&' +
+'eventtype=earthquake';
+
+/**
+* Acquires earthquakes around the specified latitude and longitude within
+* a given radius.
+*
+* @param {number} lat Center latitude.
+* @param {number} lng Center longitude.
+* @param {number} radius Radius around the coordinates
+* @param {(!Date|string)} startDate Start Date.
+* @param {(!Date|string)=} endDate End Date. Defaults to the current date.
+*
+* @return {!Promise<!Object>}
+*/
+export function getEvents(lat, lng, radius, startDate, 
+  endDate = new Date()) {
+    const xhr = new XMLHttpRequest();
+    const start = (new Date(startDate)).toISOString();
+    const end = (new Date(endDate)).toISOString();
+    
+    return new Promise((resolve, reject) => {
+      xhr.open('GET',
+      `${URL}&starttime=${start}&endtime=${end
+      }&latitude=${lat}&longitude=${lng}&maxradiuskm=${radius}`);
+      
+      xhr.onload = ({ target }) => {
+        const { status, response } = target;
+        
+        if (status >= 200 && status < 400) {
+          try {
+            const parsed = JSON.parse(response);
+            resolve(parsed);
+          } catch (e) {
+            reject(e);
+          }
+        } else {
+          reject(target);
+        }
+      };
+      
+      xhr.onerror = ({ target }) => {
+        reject(target);
+      };
+      
+      xhr.send();
+    });
+  }
+  
