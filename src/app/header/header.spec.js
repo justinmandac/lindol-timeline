7e42e02a401a00dbf1a50ab01c50e24452bf38f4
Bien Justin Mandac <justinmandac@gmail.com>
2017-09-10 13:51:35 +0800

    Initial tests for <Header />

--- src/app/header/header.spec.js
+++ src/app/header/header.spec.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { expect } from 'chai';
+import React, {Component} from 'react';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
 import { shallow } from 'enzyme';
 import Header from './header';
+
+chai.use(chaiEnzyme());
 
 describe('<Header />', () => {
   it('should have the base classes', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.find('.header')).to.be.equal(1);
+    expect(wrapper.is('.header.app-header')).to.be.true;
   });
 });
