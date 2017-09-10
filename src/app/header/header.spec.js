026dc478a2731813bb6ed0870aadada24df2820e
Bien Justin Mandac <justinmandac@gmail.com>
2017-09-10 14:00:07 +0800

    More tests for <Header /> for checking the setting of the .-visible class

--- src/app/header/header.spec.js
+++ src/app/header/header.spec.js
@@ -11,4 +11,14 @@ describe('<Header />', () => {
     const wrapper = shallow(<Header />);
     expect(wrapper.is('.header.app-header')).to.be.true;
   });
+
+  it('should be visible when the visible prop is set to true', () => {
+    const wrapper = shallow(<Header visible={true} />);
+    expect(wrapper.is('.-visible')).to.be.true;
+  });
+
+  it('should not be visible when the visible prop is set to false', () => {
+    const wrapper = shallow(<Header visible={false} />);
+    expect(wrapper.is('.-visible')).to.be.false;
+  });
 });
