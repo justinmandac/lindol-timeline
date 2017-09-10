df4963cf769996df8fff1521ae6348f91b3d4f5a
Bien Justin Mandac <justinmandac@gmail.com>
2017-09-10 15:56:36 +0800

    Respond to ExpandTrigger clicks by toggling ExpandTrigger.expanded via onClick event handler

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -13,13 +13,26 @@ export default class App extends Component{
     };
   }
 
+  /** 
+   * Handles clicks on the <BottomControls />'s expand trigger to toggle the
+   * said component's visibility.
+   */
+  handleBottomTriggerClick() {
+    this.setState(
+      (prevState, props) => Object.assign(prevState, {
+         bottomControlsExpanded: !prevState.bottomControlsExpanded }));
+  }
+
   render() {
     const { headerVisible, bottomControlsExpanded } = this.state;
     return (
       <div className="app">
         <Header visible={headerVisible} />
         <Map />
-        <BottomControls expanded={bottomControlsExpanded} />
+        <BottomControls 
+          expanded={bottomControlsExpanded}
+          onClick={this.handleBottomTriggerClick.bind(this)}
+        />
       </div>
     );
   }
